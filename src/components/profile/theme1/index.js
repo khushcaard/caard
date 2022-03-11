6ef7077cbcaad26d9e24dc84c13e5e87d21de44b
khushmanvar <khushmanvar@gmail.com>
2022-03-11 11:49:00 +0530

    Added profile

--- src/components/profile/theme1/index.js
+++ src/components/profile/theme1/index.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import c from './theme1.module.scss';
 import {AppIcon, YoutubeIcon} from '../../shared/svg';
 import CardMedia from '@mui/material/CardMedia';
@@ -7,24 +7,19 @@ import {videoURL} from '../../../helper/theme';
 import {API_URLS} from "../../../config/api_urls/api_urls";
 
 const Theme1 = ({id, data, ...props}) => {
-   
     const [play, setPlay] = useState(false)
-    const [url, setUrl] = useState("https://media.istockphoto.com/photos/dotted-grid-paper-background-texture-seamless-repeat-pattern-picture-id1320330053?b=1&k=20&m=1320330053&s=170667a&w=0&h=XisfN35UnuxAVP_sjq3ujbFDyWPurSfSTYd-Ll09Ncc=")
-    useEffect(() => {
-        fetch("https://api.unsplash.com/photos/random/?client_id=ggH1r9hjKwGNiw2nhHuZJ9eQoQU72Qgydty47Y06vZs")
-            .then(res => res.json())
-            .then((result) => setUrl(result.urls.small))
-    }, [])
+    const videoId = data.FeaturedVideo.split("?v=")[1];
+    const thumb = "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg"
     
     return(
-    <div style={{ backgroundImage: `url(${url})`}} className={classNames(c.theme1)}>
-            
-        <div className={classNames(c.filter)}>
-            <div className={classNames(c.profileBgContainer)} />
-            <center><img className={classNames(c.profileImage, "rounded-circle")} src={data?.PersonalInfo?.ImageLocation ? `data:image/png;base64,${data?.PersonalInfo?.ImageLocation}` : require('../../../assets/images/Profile.png')} alt="profile"/></center>
+        <div className={classNames(c.theme1)}>
+            <div className={classNames(c.profileBgContainer)}>
+            </div>
+            <center>
+                <img className={classNames(c.profileImage, "rounded-circle")} src={data?.PersonalInfo?.ImageLocation ? `data:image/png;base64,${data?.PersonalInfo?.ImageLocation}` : require('../../../assets/images/Profile.png')} alt="profile"/>
+            </center>
             <div className={classNames(c.profileName, "text-center")}>{data?.FullName || "Update your name"}</div>
             <div className={classNames(c.cityText, "text-center")}>{data?.PersonalInfo?.Location || "Update your location"}</div>
-            <div className={classNames(c.cityText, "text-center")}>{data?.PersonalInfo?.Work || "Update your work"}</div>
 
             <div className={classNames(c.boxContainer)}>
                 <div className={classNames(c.boxTextContainer)}>
@@ -40,14 +35,20 @@ const Theme1 = ({id, data, ...props}) => {
                             (data.SocialLinks).map((item)=>{
                                 return(
                                     
-                                        <center key={item?.Order}>
+                                        <center className={classNames("mt-1")} key={item?.Order}>
                                             <a href={`https://${item?.URL}`} target="_blank" rel="noopener noreferrer" className={c.anchor}>
                                                 <img className={classNames(c.socialImage)} src={`${API_URLS.whiteAssets}${item?.Name}.svg`} alt={item?.Name}/>
                                             </a>
+        
                                         </center>
                                 )
                             })
                         }
+                        <center className={classNames("mt-1")} >
+                            <a href="tel:9510816858" target="_blank" rel="noopener noreferrer" className={c.anchor}>
+                                <img className={classNames(c.socialImage)} src={require("../../../assets/images/contact.png")} />
+                            </a>
+                        </center>
                     </div>
                 }
 
@@ -56,10 +57,9 @@ const Theme1 = ({id, data, ...props}) => {
                     <div className="my-5">
                         {
                             play ?
-                            <CardMedia component="iframe" allow="autoplay" allowFullScreen="allowfullscreen" className={classNames(c.videoPlayer)} 
-                                src={`${videoURL(data?.FeaturedVideo)}?autoplay=1&mute=1`}/>
+                            <CardMedia component="iframe" allow="autoplay" allowFullScreen="allowfullscreen" className={classNames(c.videoPlayer)} src={`${videoURL(data?.FeaturedVideo)}?autoplay=1&mute=1`}/>
                             :
-                            <div className={classNames(c.video)} style={{backgroundImage: 'url(' + require("../../../assets/images/video.png")+')'}}>
+                            <div className={classNames(c.video)} style={{backgroundImage: `url(${thumb})`}}>
                                 {
                                     data?.PersonalInfo?.ImageLocation ? 
                                     <div className={classNames(c.icon)} style={{backgroundImage: 'url('+ `data:image/png;base64,${data?.PersonalInfo?.ImageLocation}`+')'}}></div>
@@ -80,7 +80,6 @@ const Theme1 = ({id, data, ...props}) => {
                 <center><div className={classNames(c.text)}>Developed by Caard©</div></center>
             </div>
             
-            </div>
             </div>
         </div>
     )
