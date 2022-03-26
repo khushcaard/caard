c12f6ed593f8ff53d5e455defaad51f3e586579e
khushmanvar <khushmanvar@gmail.com>
2022-03-26 22:10:31 +0530

    added new icon

--- src/components/profile/theme2/index.js
+++ src/components/profile/theme2/index.js
@@ -102,9 +102,7 @@ const Theme2 = ({id, data, ...props}) => {
                                     </div>
                             }
                             </div>
-                            ) : (
-                                <div style={{ width:100, height:100}}></div>
-                            )
+                            ) : (null)
                         }
 
                         {
