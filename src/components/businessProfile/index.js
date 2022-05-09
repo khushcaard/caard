381529ea13cb68aabef2e10f3673ca07b94bf410
khushmanvar <khushmanvar@gmail.com>
2022-05-09 16:12:58 +0530

    Add: Route for business profile

---
+++ src/components/businessProfile/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Theme1 from './theme1';
+import Theme1Bg from './theme1/theme1WithBg';
+import Theme2 from './theme2';
+import Theme2Bg from './theme2';
+
+const BusinessTheme = ({theme, id, data, iurl, ...props}) => {
+    if(theme == "Light" || theme == "White") {
+        if(data.BackgroundImage) {
+            return (<Theme1Bg data={data} id={id}/>)
+        } else {
+            return(<Theme1 data={data} id={id}/>)
+        } 
+    } 
+    else if (theme == "Dark") {
+        if(data.BackgroundImage) {
+            return(<Theme2Bg data={data} id={id} />)
+        } else {
+            return(<Theme2 data={data} id={id}/>)
+        } 
+        
+    }
+}
+export default BusinessTheme;
